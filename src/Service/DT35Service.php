190fd6352db714ea3daf7dcbb85187d349e687d8
Rakesh Kumar <rkhregar@gmail.com>
2023-10-24 16:46:57 +0530

    Meaasge based on current time complete

--- src/Service/DT35Service.php
+++ src/Service/DT35Service.php
@@ -11,7 +11,21 @@ class DT35Service {
    * {@inheritDoc}
    */
   public static function messageBasedOnTime() : string {
-    return 'Good Morning';
+    $message = 'Good Morning';
+    $time = intval(date('Hi'));
+    if ($time >= 600 && $time < 1200) {
+      $message = 'Good Morning';
+    }
+    if ($time > 1200 && $time <= 1600) {
+      $message = 'Good Afternoon';
+    }
+    if ($time > 1600 && $time <= 2100) {
+      $message = 'Good Evening';
+    }
+    if ($time > 2100 && $time < 2359 || $time >= 0 && $time < 600) {
+      $message = 'Good Night';
+    }
+    return $message;
   }
 
 }
